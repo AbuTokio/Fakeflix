1a65c71c94403ea4457bdfd48fa6aff8bed13e88
AbuTokio <bunyaminxkara@gmail.com>
2025-09-05 15:24:48 +0200

    add: button component

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -8,8 +8,8 @@ export default function Button({ filled, label, imgUrl }: ButtonProps) {
   return (
     <button
       className={`${
-        filled ? "bg-red-600" : "bg-transparent border-2 border-red-600"
-      } text-white text-xl font-bold font-nunito p-4 flex justify-between items-center gap-2 rounded-md active:opacity-80 cursor-pointer`}>
+        filled ? "bg-red-600" : "bg-transparent border-2 border-red-600 active:bg-red-600"
+      } text-white text-xl font-bold font-nunito p-4 flex justify-between items-center gap-2 rounded-md active:opacity-70 cursor-pointer`}>
       {label}
       {imgUrl && <img className="" src={imgUrl} alt={label} />}
     </button>
