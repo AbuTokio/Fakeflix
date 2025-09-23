436233be4da3437dd6cc88bc8494bacbc39fa492
nucito101 <andrenucitoschmidt@gmail.com>
2025-09-23 11:40:32 +0200

    Update Layout.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet, useLocation } from "react-router"
 import Header from "../components/header/Header"
+import useScrollToTop from "../utility/Scroll"
 
 export default function Layout() {
   const path = useLocation()
   const noHeader = ["/"]
+  useScrollToTop()
 
   return (
     <>
