1ee30d91747a76f8777291e7c99e6d8556256df2
AbuTokio <bunyaminxkara@gmail.com>
2025-09-25 11:19:19 +0200

    fixed an error

--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -42,14 +42,14 @@ export default function Searchbar() {
           <svg
             className="size-6 absolute top-3 right-3 text-black cursor-pointer"
             stroke="currentColor"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg">
             <path
               d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-              stroke-linejoin="round"
-              stroke-linecap="round"></path>
+              strokeLinejoin="round"
+              strokeLinecap="round"></path>
           </svg>
         </button>
       </div>
