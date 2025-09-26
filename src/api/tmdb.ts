cb29561457fb31ce2b70cc8fce560111181663b9
nucito101 <andrenucitoschmidt@gmail.com>
2025-09-26 12:37:25 +0200

    Update tmdb.ts

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -3,6 +3,6 @@ import axios from "axios"
 export const tmdb = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: `Bearer ${import.meta.env.VITE_TMDB_API_TOKEN}`,
+    Authorization: `Bearer ${import.meta.env.TMDB_API_KEY}`,
   },
 })
